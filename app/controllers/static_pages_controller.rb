f09aef4bb2f9f8071680b824a14c518642b26cfa
ducmanh2111 <ducmanh2111@gmail.com>
2019-06-12 13:36:30 +0700

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,13 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-
-
-  def contact
+	def home
 	end
-  def about
 
-  end
+	def help
+	end
+
+	def contact
+	end
+	
+	def about
+	end
 end
